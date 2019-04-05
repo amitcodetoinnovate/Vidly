706b9119609df946ca7661567522f7ae565c585e
Amit Kumar Verma <amitsamrat563@gmail.com>
2019-04-06 01:45:26 +0530

    Initial commit

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies= GetMovie();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovie()
+        {
+            return new List<Movie>
+            {
+                new Movie{ID = 1, Name ="Shrek"},
+                new Movie{ID = 2, Name ="Wall E"}
+            };
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
